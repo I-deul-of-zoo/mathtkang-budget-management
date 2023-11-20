95c29da4ad7da4f8a3a4ecf15f56ed6070eaf092
sgaeng <ksge1124@gmail.com>
2023-11-21 01:18:41 +0900

    :sparkles:Feat : 예산 설정 API (POST, PUT) 추가  #7
    Close #7

--- budgets/views.py
+++ budgets/views.py
@@ -5,7 +5,43 @@ from budgets.models import Category, Budget
 from budgets.serializers import CategorySerializer
 
 class CategoryList(APIView):
-    def get(self, request, format=None):
+    def get(self, request):
         categories = Category.objects.all()
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data, 
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(
+            serializer.errors, 
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def put(self, request):
+        try:
+            category_id = request.data.get('id')
+            category = Category.objects.get(pk=category_id)
+        except Category.DoesNotExist:
+            return Response(
+                {'error': 'Category not found'}, 
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = CategorySerializer(category, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(
+            serializer.errors, 
+            status=status.HTTP_400_BAD_REQUEST
+        )
